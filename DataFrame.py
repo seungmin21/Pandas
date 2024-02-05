b60fb5fe6f4e7ccdf87e4760df59afc308e65c2e
ohseungmin <ohmin2886@gmail.com>
2024-02-05 17:59:27 +0900

    데이터 열 선택

--- DataFrame.py
+++ DataFrame.py
@@ -4,4 +4,7 @@ data = {'Name': ['Alice', 'Bob', 'Charlie'],
         'Age': [25, 30, 35],
         'City': ['New York', 'San Francisco', 'Los Angeles']}
 df = pd.DataFrame(data)
-print(df)
+#print(df)
+
+# 열 선택
+print(df['Name'])
